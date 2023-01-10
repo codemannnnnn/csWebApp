99ebf517d6839c9ba03db9d8284947e5e3fe99fe
Cody Branson <branson@glsolutions.com>
2023-01-10 15:10:33 -0700

    added new code with fixed changes.

--- index.js
+++ index.js
@@ -1,57 +1,27 @@
 const axios = require("axios");
 const { DOMParser } = require("xmldom");
+const qs = require("querystring");
 
-const data = require("./spCalls");
-
-var text =
-  '<?xml version="1.0" encoding="utf-8"?>' +
-  '<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">' +
-  "<soap:Body>" +
-  '   <ProcessDocument xmlns="http://glsuite.us/">' +
-  "    <docXml>" +
-  '<root xmlns="">' +
-  "<Requests>" +
-  '<Request RequestTypeID="2" ResponseOnly="True">' +
-  ' <Object ObjectID="1" ObjectPK="1000009">' +
-  '   <Object ObjectID="34" ActionTypeID="1">' +
-  '     <Property ObjectPK="76" Name="ObjectTypeID" ActionTypeID="2">10934</Property>' +
-  "   </Object>" +
-  " </Object>" +
-  "</Request>" +
-  "</Requests>" +
-  '<UserToken EntityID="1260091" FirstName="AKCCED_SI" MiddleName="" LastName="WebService" EntityName="AKCCED_SI WebService" EntitySecurity="False" NTUserName="AKCCED_SI_WebService" AdhocReporting="False" AdhocReportingKey="" SecureLoginCode="" QuestionsHistoryID="-1" SystemMFATypeID="0" UserMFATypeID="0" MFASecretOPVID="-1" MFATimeout="0" Expiration="2043-01-06T06:36:13.971">' +
-  '<UserGroup EntityName="All Users Group">999989</UserGroup>' +
-  '<UserGroup EntityName="AKCCED_SI_WebService">-3</UserGroup><![CDATA[cUTegifXx0FUGdfu/vZqaFlcoED+sGLruRroNsvbvcDdUyYtpS9v1IhncfLxDhmfvM+ipECMUgc=]]></UserToken>' +
-  "</root>" +
-  " </docXml>" +
-  "</ProcessDocument>" +
-  "  </soap:Body>" +
-  "</soap:Envelope>";
-
-var parser = new DOMParser();
-var xmlDoc = parser.parseFromString(text, "text/xml");
-
-xmlDoc.documentElement.setAttribute("x", "y");
-xmlDoc.documentElement.setAttributeNS("./lite", "c:x", "y2");
-// console.info(xmlDoc);
-
-const getData = async (doc) => {
-  try {
-    const url =
-      "https://akcceddev.glsuite.us/BusinessTier/Public/RuleProcessor.asmx";
-
-    var res = await axios.post(url, doc, {
-      headers: {
-        contentType: "text/xml",
-        soapAction: "http://glsuite.us/ProcessDocument",
-      },
-    });
-    //   console.log(res.data);
-  } catch (err) {
-    console.log(err.message);
-  }
+var data =
+  '<?xml version="1.0" encoding="utf-8"?>\r\n<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">\r\n  <soap:Body>\r\n    <ProcessDocument xmlns="http://glsuite.us/">\r\n     <docXml>\r\n<root xmlns="">\r\n    <Requests>\r\n    <Request RequestTypeID="2" ResponseOnly="True">\r\n    <Object ObjectID="1" ObjectPK="1000009">\r\n        <Object ObjectID="34" ActionTypeID="1">\r\n        <Property ObjectPK="76" Name="ObjectTypeID" ActionTypeID="2">10934</Property>\r\n        </Object>\r\n    </Object>\r\n    </Request>\r\n    </Requests>\r\n<UserToken EntityID="1260091" FirstName="AKCCED_SI" MiddleName="" LastName="WebService" EntityName="AKCCED_SI WebService" EntitySecurity="False" NTUserName="AKCCED_SI_WebService" AdhocReporting="False" AdhocReportingKey="" SecureLoginCode="" QuestionsHistoryID="-1" SystemMFATypeID="0" UserMFATypeID="0" MFASecretOPVID="-1" MFATimeout="0" Expiration="2043-01-06T06:36:13.971">\r\n<UserGroup EntityName="All Users Group">999989</UserGroup>\r\n<UserGroup EntityName="AKCCED_SI_WebService">-3</UserGroup><![CDATA[cUTegifXx0FUGdfu/vZqaFlcoED+sGLruRroNsvbvcDdUyYtpS9v1IhncfLxDhmfvM+ipECMUgc=]]></UserToken>\r\n</root>\r\n </docXml>\r\n</ProcessDocument>\r\n  </soap:Body>\r\n</soap:Envelope> ';
+var url = "https://akcceddev.glsuite.us/BusinessTier/Public/RuleProcessor.asmx";
+var config = {
+  headers: {
+    "Content-Type": "text/xml; charset=utf-8",
+    SOAPAction: '"http://glsuite.us/ProcessDocument"',
+  },
+  data: data,
 };
 
-getData(xmlDoc);
+const grabData = async () => {
+  var res = await axios
+    .post(url, data, config)
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+};
 
-// console.log(data.sp1);
+grabData();
